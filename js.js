258586049c4b15398cb3109bb466e81edba62cdf
MagnusJorg <eaa23mojoe@students.eaaa.dk>
2024-12-10 16:50:32 +0100

    f

--- js.js
+++ js.js
@@ -1,14 +1,44 @@
 // Henter Elementerne
-const burgerIcon = document.getElementById('burgerIcon');
+const burgerIkon = document.getElementById('burgerIkon');
 const menu = document.getElementById('menu');
-const closeMenu = document.getElementById('closeMenu');
+const lukMenu = document.getElementById('lukMenu');
+
+const holdButton = document.getElementById('holdButton');
+const holdMenu = document.getElementById('holdMenu');
+
+const blivMedlemButton = document.getElementById('blivMedlemButton');
+const blivMedlemMenu = document.getElementById('blivMedlemMenu');
+
+const faciliteterButton = document.getElementById('faciliteterButton');
+const faciliteterMenu = document.getElementById('faciliteterMenu');
+
+const kontaktButton = document.getElementById('kontaktButton');
+const kontaktMenu = document.getElementById('kontaktMenu');
 
 // Åbn menuen, når burger-ikonet klikkes
-burgerIcon.addEventListener('click', () => {
+burgerIkon.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 // Luk menuen når x klikkes
-closeMenu.addEventListener('click', () => {
+lukMenu.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+// Funktion til at toggle visning
+function toggleMenu(button, menu) {
+    button.addEventListener('click', (event) => {
+        event.preventDefault(); // Forhindre standard link-adfærd
+        if (menu.style.display === "none") {
+            menu.style.display = "block"; // Vis menuen
+        } else {
+            menu.style.display = "none"; // Skjul menuen
+        }
+    });
+}
+
+// funktion til hvert menupunkt
+toggleMenu(holdButton, holdMenu);
+toggleMenu(blivMedlemButton, blivMedlemMenu);
+toggleMenu(faciliteterButton, faciliteterMenu);
+toggleMenu(kontaktButton, kontaktMenu);
